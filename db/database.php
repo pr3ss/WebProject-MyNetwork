fd9f8dacf2ab42dc36b349be373aa3ab4fe558c8
alex presepi <alex.presepi@studio.unibo.it>
2022-12-09 16:39:35 +0100

    Add css and db basic file

---
+++ db/database.php
@@ -0,0 +1,23 @@
+<?php
+class DatabaseHelper{
+    private $db;
+
+    public function __construct($servername, $username, $password, $dbname, $port){
+        $this->db = new mysqli($servername, $username, $password, $dbname, $port);
+        if ($this->db->connect_error) {
+            die("Connection failed: " . $db->connect_error);
+        }        
+    }
+
+    public function checkLogin($username, $password){
+        $query = "SELECT idautore, username, nome FROM autore WHERE attivo=1 AND username = ? AND password = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('ss',$username, $password);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }    
+
+}
+?>
