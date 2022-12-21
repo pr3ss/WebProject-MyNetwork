1e6bda50d6672c70db5c9560f647f91a61ae05b5
Simone Lugaresi <simone.lugaresi@studio.unibo.it>
2022-12-21 11:45:08 +0100

    add some test login page

--- template/login-base.php
+++ template/login-base.php
@@ -1,31 +1,35 @@
 <!DOCTYPE html>
 <html lang="it">
+
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?php echo $templateParams["title"]; ?></title>
-    <link rel="stylesheet" type="text/css" href="./css/style.css" />
-
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title><?php echo $templateParams["title"]; ?></title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="./css/styleLogin_base.css" />
 </head>
-<body>
-    <header>
-        <h1>Social-Network</h1>
-    </header>
-    <main>
-        <!-- Generate with js -->
-    </main>
-    <footer>
-        <p>Anno 2022/2023</p>
-    </footer>
 
-    <?php
-    if(isset($templateParams["js"])):
-        foreach($templateParams["js"] as $script):
-    ?>
-        <script src="<?php echo $script; ?>"></script>
-    <?php
-        endforeach;
-    endif;
-    ?>
-    
+<body class="text-center">
+  <header>
+    <img class="mb-4" src="./img/logo.png" alt="nissan logo" width="72" height="57">
+    <h1>Social-Network</h1>
+  </header>
+  <main>
+  </main>
+  <footer>
+    <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+  </footer>
+
+  <?php
+  if (isset($templateParams["js"])):
+    foreach ($templateParams["js"] as $script):
+  ?>
+  <script src="<?php echo $script; ?>"></script>
+  <?php
+    endforeach;
+  endif;
+  ?>
+
 </body>
+
 </html>
