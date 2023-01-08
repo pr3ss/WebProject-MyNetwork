898db53aa4552172960e011c2838a79329a7c2f0
Simone Lugaresi <simone.lugaresi@studio.unibo.it>
2023-01-08 21:27:45 +0100

    fix dbCreate ;

--- db/DBcreate.sql
+++ db/DBcreate.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `web-socialnetwork` 
+CREATE DATABASE  IF NOT EXISTS `web-socialnetwork` ;
 USE `web-socialnetwork`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Linux (x86_64)
 --
