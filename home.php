34ecff3c9d48c243fb678380d05ac90a43bf4cac
alex presepi <alex.presepi@studio.unibo.it>
2023-01-14 16:53:00 +0100

    Fix home

--- home.php
+++ home.php
@@ -7,6 +7,8 @@ $templateParams["title"] = "Home";
 if($dbh->login_check()){
     $templateParams["username"] = $_SESSION["username"];
     $templateParams['js'] = array("https://unpkg.com/axios/dist/axios.min.js", "js/base.js");
+    //$templateParams['posts'] = db call;
+    
 
     require 'template/base.php';
 }else{ //non autorizzato
