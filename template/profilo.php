bd06e78421bc160a7daf1c06e55e22ad3be2014a
Simone Lugaresi <simone.lugaresi@studio.unibo.it>
2023-01-22 17:28:04 +0100

    fix profilo

--- template/profilo.php
+++ template/profilo.php
@@ -2,29 +2,29 @@
     <div class="row bg-white ">
         <div class="col-2"></div>
         <div class="col-8 d-flex flex-column">
-            <img src="<?php echo IMG_DIR . $templateParams["info"][0]["foto_profilo"] ?>" alt="..." class="img-fluid rounded-circle" style="aspect-ratio: 1; object-fit: cover;">
+            <img src="<?php echo IMG_DIR . $templateParams["info"][0]["foto_profilo"] ?>" alt="..."
+                class="img-fluid rounded-circle" style="aspect-ratio: 1; object-fit: cover;">
         </div>
         <div class="col-2"></div>
         <div class="col-12">
             <div class="row justify-content-center">
-                <p>
+                <h2>
                     <?php echo $templateParams["info"][0]["username"] ?>
-                </p>
+                </h2>
             </div>
         </div>
         <div class="col-6">
             <div class="row justify-content-center m-2">
-                <?php echo "<button type='button' class='btn btnshadow ' onclick='view_seguiti_follower(" . json_encode($templateParams["follower"]) . ")'><i class='fa-solid fa-user'>
+                <?php echo "<button type='button' class='btn p-0' onclick='view_seguiti_follower(" . json_encode($templateParams["follower"]) . ")'><i class='fa-solid fa-user' style='font-size: 110%;'>
                     " . count($templateParams["follower"]) . "  FOLLOWER</i></button>"
                     ?>
             </div>
         </div>
         <div class="col-6">
             <div class="row justify-content-center m-2">
-                <?php echo "<button type='button' class='btn btnshadow btn-dark' onclick='view_seguiti_follower(" . json_encode($templateParams["seguiti"]) . ")'><i class='fa-solid fa-user'>
+                <?php echo "<button type='button' class='btn p-0' onclick='view_seguiti_follower(" . json_encode($templateParams["seguiti"]) . ")'><i class='fa-solid fa-user' style='font-size: 110%;'>
                  " . count($templateParams["seguiti"]) . "  SEGUITI</i></button>"
                     ?>
-                <!--  <button type="button" class="btn btnshadow btn-dark" onclick="viewSeguiti()"><i class="fa-solid fa-user">  SEGUITI</i></button> -->
             </div>
         </div>
         <div class="col-12">
@@ -41,9 +41,9 @@
             <div class="container-fluid bg-white p-0" style="border-radius: 10px;">
                 <div class="row d-flex justify-content-center pl-3 pr-3">
                     <div class="col-4 d-flex flex-column justify-content-center">
-                        <button type="button" class="btn p-0 w-100 btnLike_<?php echo $post['id'] ?>     <?php if ($post['asliked'] != null) {
-                                    echo "btn-danger";
-                                } ?>" onclick="likePost(<?php echo $post['id'] ?>)"><i class="fa-solid fa-heart"
+                        <button type="button" class="btn p-0 w-100 btnLike_<?php echo $post['id'] ?>         <?php if ($post['asliked'] != null) {
+                                        echo "btn-danger";
+                                    } ?>" onclick="likePost(<?php echo $post['id'] ?>)"><i class="fa-solid fa-heart"
                                 style="font-size: 150%;">
                                 <?php echo $post["miPiace"] ?>
                             </i></button>
@@ -51,7 +51,7 @@
                     <div class="col-4 d-flex flex-column justify-content-center">
                         <button type="button" class="btn w-100" onclick="openPost(<?php echo $post['id'] ?>)"><i
                                 class="fa-solid fa-comment" style="font-size: 150%;">
-                                    <?php echo $post["nCommenti"] ?>
+                                <?php echo $post["nCommenti"] ?>
                             </i></button>
                     </div>
                     <div class="col-4 d-flex flex-column justify-content-center">
--- template/profilo_other_user.php
+++ template/profilo_other_user.php
@@ -2,26 +2,27 @@
     <div class="row bg-white ">
         <div class="col-2"></div>
         <div class="col-8 d-flex flex-column">
-            <img src="<?php echo IMG_DIR . $templateParams["info"][0]["foto_profilo"] ?>" class="img-fluid rounded-circle" style="aspect-ratio: 1; object-fit: cover;">
+            <img src="<?php echo IMG_DIR . $templateParams["info"][0]["foto_profilo"] ?>"
+                class="img-fluid rounded-circle" style="aspect-ratio: 1; object-fit: cover;">
         </div>
         <div class="col-2"></div>
         <div class="col-12">
             <dic class="row justify-content-center">
-                <p>
+                <h2>
                     <?php echo $templateParams["info"][0]["username"] ?>
-                </p>
+                </h2>
             </dic>
         </div>
         <div class="col-6">
             <div class="row justify-content-center m-2">
-                <?php echo "<button type='button' class='btn btnshadow ' onclick='view_seguiti_follower(" . json_encode($templateParams["follower"]) . ")'><i class='fa-solid fa-user'>
+                <?php echo "<button type='button' class='btn p-0' onclick='view_seguiti_follower(" . json_encode($templateParams["follower"]) . ")'><i class='fa-solid fa-user' style='font-size: 110%;'>
                     " . count($templateParams["follower"]) . "  FOLLOWER</i></button>"
                     ?>
             </div>
         </div>
         <div class="col-6">
             <div class="row justify-content-center m-2">
-                <?php echo "<button type='button' class='btn btnshadow btn-dark' onclick='view_seguiti_follower(" . json_encode($templateParams["seguiti"]) . ")'><i class='fa-solid fa-user'>
+                <?php echo "<button type='button' class='btn p-0' onclick='view_seguiti_follower(" . json_encode($templateParams["seguiti"]) . ")'><i class='fa-solid fa-user' style='font-size: 110%;'>
                  " . count($templateParams["seguiti"]) . "  SEGUITI</i></button>"
                     ?>
             </div>
@@ -53,9 +54,9 @@
             <div class="container-fluid p-0 bg-white " style="border-radius: 10px;">
                 <div class="row d-flex justify-content-center pl-3 pr-3">
                     <div class="col-6 d-flex flex-column justify-content-center">
-                        <button type="button" class="btn p-0 w-100 btnLike_<?php echo $post['id'] ?>     <?php if ($post['asliked'] != null) {
-                                    echo "btn-danger";
-                                } ?>" onclick="likePost(<?php echo $post['id'] ?>)"><i class="fa-solid fa-heart"
+                        <button type="button" class="btn p-0 w-100 btnLike_<?php echo $post['id'] ?>             <?php if ($post['asliked'] != null) {
+                                            echo "btn-danger";
+                                        } ?>" onclick="likePost(<?php echo $post['id'] ?>)"><i class="fa-solid fa-heart"
                                 style="font-size: 150%;">
                                 <?php echo $post["miPiace"] ?>
                             </i></button>
@@ -63,7 +64,7 @@
                     <div class="col-6 d-flex flex-column justify-content-center">
                         <button type="button" class="btn p-0 w-100" onclick="openPost(<?php echo $post['id'] ?>)"><i
                                 class="fa-solid fa-comment" style="font-size: 150%;">
-                                    <?php echo $post["nCommenti"] ?>
+                                <?php echo $post["nCommenti"] ?>
                             </i></button>
                     </div>
                 </div>
