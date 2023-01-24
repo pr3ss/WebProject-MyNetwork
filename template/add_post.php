f0e0b2b11ed54ddb222331389ee9be629a344d8e
Simone Lugaresi <simone.lugaresi@studio.unibo.it>
2023-01-24 14:41:29 +0100

    refactor e mockup.png

--- template/add_post.php
+++ template/add_post.php
@@ -5,13 +5,16 @@
 
     </div>
     <div class="row justify-content-center mr-5 ml-5 mt-1">
-        <input id="tmp_post_text" type="text" id="addANote" class="form-control" placeholder="Add text" />
-        <input id="tmp_post_luogo" type="text" id="addANote" class="form-control" placeholder="Luogo" />
+        <label for="tmp_post_text" style="display: none;">Add text</label>
+        <textarea id="tmp_post_text"  class="form-control" placeholder="Add text" ></textarea>
+        <label for="tmp_post_luogo" style="display: none;">Add place</label>
+        <input id="tmp_post_luogo" type="text" class="form-control" placeholder="Luogo" />
+        <label for="tmp_post_cat" style="display: none;">Choose category</label>
         <select id="tmp_post_cat" class="form-select w-100 rounded-pill mt-2" aria-label="Default select example" style="color: #6C757D; text-align: center; border: #6C757D solid 1px;">
             <?php foreach($templateParams["categorie"] as $categoria): ?>
                 <option  value="<?php echo $categoria["id"] ?>" <?php if($categoria['id']==1){echo "selected";} ?>><?php echo $categoria["titolo"] ?></option>
             <?php endforeach;?>
         </select>
-        <div class="btn btnMagico active mt-4 w-100" onclick="upload_post()">Salva</div>
+        <input class="btn btnMagico active mt-4 w-100"  type="button" onclick="upload_post()" value="salva">
     </div>
 </div>
--- template/base.php
+++ template/base.php
@@ -92,6 +92,7 @@
                     <div class="row mt-3 mb-1 w-100  ">
                         <div class="col-12 d-flex flex-row align-items-center">
                             <i class="fa-solid fa-magnifying-glass pr-2 pl-2"></i>
+                            <label for="input_search_user" style="display: none;">Search user</label>
                             <input id="input_search_user" class="form-control " type="search" placeholder="Search"
                                 aria-label="Search" oninput="ricerca_user()">
                         </div>
